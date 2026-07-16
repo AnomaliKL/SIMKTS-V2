f775b186cae2e2a949426928f9d46466cfc587d3
AnomaliKL <khaikalanjingbangsat@gmail.com>
2026-07-16 21:05:23 +0700

    insya allah terakhir

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,20 +23,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         URL::forceScheme('https');
-
-        if (app()->runningInConsole()) {
-            return;
-        }
-
-        // Kode di bawah ini hanya berjalan jika diakses via web browser normal
-        if (Schema::hasTable('banks')) {
-            $setup = Bank::first();
-
-            if ($setup && $setup->smtp_email && $setup->smtp_app_password) {
-                Config::set('mail.mailers.smtp.username', $setup->smtp_email);
-                Config::set('mail.mailers.smtp.password', $setup->smtp_app_password);
-                Config::set('mail.from.address', $setup->smtp_email);
-            }
-        }
     }
 }
