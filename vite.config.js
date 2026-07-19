8e9ebc7257cdbca8a15af2f391c0eb2274705920
AnomaliKL <khaikalanjingbangsat@gmail.com>
2026-07-19 18:00:12 +0700

    perbaikan

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,34 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import { bunny } from 'laravel-vite-plugin/fonts';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//             fonts: [
+//                 bunny('Instrument Sans', {
+//                     weights: [400, 500, 600],
+//                 }),
+//             ],
+//         }),
+//         tailwindcss(),
+//     ],
+//     css: {
+//         lightningcss: {
+//             // Mengabaikan error/warning untuk sintaks CSS modern yang belum dikenali minifier
+//             errorRecovery: true 
+//         }
+//     },
+//     server: {
+//         watch: {
+//             ignored: ['**/storage/framework/views/**'],
+//         },
+//     },
+// });
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
